77d2b8b6279e85fecd295e5791a83f95c675daa7
David Zhang <mcmadbat@gmail.com>
2018-03-25 16:26:28 -0400

    autocomplete bug fix and default mug (#24)

--- chrome/src/js/playerData.js
+++ chrome/src/js/playerData.js
@@ -9,7 +9,8 @@ function getPlayerData() {
 function findPlayerById(id) {
   let obj = {
     name: "A Player",
-    position: 0
+    position: 0,
+    team: 'N/A'
   }
 
   let player = _rawData.data.find(x => x.id == id)
@@ -17,9 +18,32 @@ function findPlayerById(id) {
   if (player) {
     obj.name = player.name
     obj.position = player.position
+    obj.team = player.team
   } else {
     console.log('cant find ' + id)
   }
   
+  return obj
+}
+
+function findPlayerByName(name) {
+  let obj = {
+    name: "A Player",
+    position: 0,
+    team: 'N/A',
+    id: 0
+  }
+
+  let player = _rawData.data.find(x => x.name == name) 
+
+  if (player) {
+    obj.name = player.name
+    obj.position = player.position
+    obj.team = player.team
+    obj.id = player.id
+  } else {
+    console.log('cant find ' + name)
+  }
+
   return obj
 }
