e86b2bd56fa651658ff6fb7731d811762be3f0df
sidhantmishra <sdhntmshr@gmail.com>
2018-03-24 22:11:42 -0400

    adding desc

--- chrome/src/js/popup.js
+++ chrome/src/js/popup.js
@@ -127,11 +127,11 @@ $( function() {
  
         return false;
       }
-    });
-    /*.autocomplete( "instance" )._renderItem = function( ul, item ) {
+    })
+    .autocomplete( "instance" )._renderItem = function( ul, item ) {
       return $( "<li>" )
         .append( "<div>" + item.label + "<br>" + item.desc + "</div>" )
         .appendTo( ul );
-    };*/
+    };
   } );
 
